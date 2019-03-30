fb30c818b6f1fe83a2cdc4f00dde186ff873b17f
Sarah Anderson <saranderson13@gmail.com>
2019-03-30 19:24:26 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   keys = name_hash.collect { |key, value| key }
   values = name_hash.collect { |key, value| value }
   last_value = 0
-  current_min = 0
+  current_min = values.first
   min_key = [keys[0]]
 
   values.each do |num|

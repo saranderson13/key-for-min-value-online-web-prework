ec300e67353fdc09ee779211e67909d334af0625
Sarah Anderson <saranderson13@gmail.com>
2019-03-30 20:07:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,14 +5,14 @@ def key_for_min_value(name_hash)
 
   keys = name_hash.collect { |key, value| key }
   values = name_hash.collect { |key, value| value }
-  # last_value = values.last
-  # current_min = values.first
   min_key = [keys[0]]
 
   values.each do |num|
     compare = values[values.index(num) + 1]
-    puts "Comparing #{keys[values.index(num)]} and #{keys[values.index(compare)]}"
-    num > compare ? min_key.unshift(keys[values.index(compare)]) : min_key.unshift(keys[values.index(num)])
+    if compare <= values.length
+      puts "Comparing #{keys[values.index(num)]} and #{keys[values.index(compare)]}"
+      num > compare ? min_key.unshift(keys[values.index(compare)]) : min_key.unshift(keys[values.index(num)])
+    end
     
     puts "#{min_key}"
     values[keys.index(min_key[0])] < values[keys.index(min_key[1])] ? min_key.pop() : min_key.shift()

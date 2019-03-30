d9994d4e6eb8fe9420921f5093ba29aaf8f5933d
Sarah Anderson <saranderson13@gmail.com>
2019-03-30 18:55:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,28 +2,20 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  min_key = []
   keys = name_hash.collect { |key, value| key }
   values = name_hash.collect { |key, value| value }
   last_value = values.last
 
   values.each do |num|
-    if keys.length > 2
-      if num > last_value
-        puts "Comparing #{num} #{keys[values.index(num)]} with #{last_value} #{keys[values.index(last_value)]}."
-        puts "Deleted #{keys[values.index(num)]}"
-        keys.delete_at(values.index(num))
-      end
-    else
-      puts "Two left"
-      puts "Comparing #{num} #{keys[values.index(num)]} with #{last_value} #{keys[values.index(last_value)]}."
-      if values.first > values.last
-        puts "Deleted #{keys[0]}"
-        keys.delete_at(0)
-      end
+    if num > last_value
+      current_min = last_value
     end
+    last_value = num
+    min_key.shift(keys.)
   end
   
   puts keys
   
-  keys[0]
+  min_key[0]
 end

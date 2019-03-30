6c60f98700a0c479b55f8a0b1bb9d83b5e12800f
Sarah Anderson <saranderson13@gmail.com>
2019-03-30 18:00:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,7 @@ def key_for_min_value(name_hash)
   puts "Comparing first with: #{last_value}"
   
   values.each do |num|
-    if values.index(num) == values.length - 1
+    if values.index(num) == (values.length - 1)
       if num < values.first
         puts "Deleted #{keys[0]}"
         keys.delete_at[0]

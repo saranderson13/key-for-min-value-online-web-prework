ea70d914288bca07a8bb1570bae72dacd0dd5ffe
Sarah Anderson <saranderson13@gmail.com>
2019-03-30 17:58:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,16 +8,17 @@ def key_for_min_value(name_hash)
   puts "Comparing first with: #{last_value}"
   
   values.each do |num|
-    if num > last_value
+    if num.index == values.last.index
+      if num < values.first
+        puts "Deleted #{keys[0]}"
+        keys.delete_at[0]
+      end
+    elsif num > last_value
       puts "Deleted #{keys[values.index(num)]}"
       keys.delete_at(values.index(num))
     end
     last_value = num
     puts "Comparing next with: #{last_value}."
-  end
-  
-  if keys.length ==2 
-    
   end
   
   keys[0]
